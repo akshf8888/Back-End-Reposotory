f26ef18968e92f36edd29ea615f33e72c36e4134
akshf8888 <602904726@qq.com>
2024-10-24 23:47:11 +0800

    Add files via upload

---
+++ addressbook.sql
@@ -0,0 +1,3 @@
+INSERT INTO `addressbook` (`name`, `age`, `telephone`, `address`, `gender`) VALUES ('hy', 19, '654321', '456', 'female');
+INSERT INTO `addressbook` (`name`, `age`, `telephone`, `address`, `gender`) VALUES ('wxr', 21, '333333', '456', 'female');
+INSERT INTO `addressbook` (`name`, `age`, `telephone`, `address`, `gender`) VALUES ('zzz', 18, '123456', '123', 'male');
